d413deba190f31c1e7bd07fda727bc1605f4ec92
Yashwanthi Anand <anandy@oregonstate.edu>
2022-07-24 16:36:05 -0700

    initialize prob & reward variables for grid world

--- grid_world.py
+++ grid_world.py
@@ -1,5 +1,14 @@
+"""
+References:
+(1) Intelligents Agents by Dr Fern course material
+(2) OpenAI Gym's FrozenLake-v0 environment (https://github.com/openai/gym/blob/master/gym/envs/toy_text/frozen_lake.py)
+"""
+
+from tkinter import Grid
 import matplotlib.pyplot as plt
 import numpy as np
+from gym.envs.toy_text import discrete
+from gym import utils
 
 # Global variables
 LEFT = 0
@@ -55,9 +64,71 @@ def generate_random_map(size=8, p=0.8):
         board[0][0] = "S"
         board[-1][-1] = "G"
         valid = is_valid(board, size)
-        print(valid)
-        print(board)
+        # print(valid)
+        # print(board)
     return ["".join(x) for x in board]
 
-generate_random_map(8, 0.8)
-print(is_valid(MAPS["4x4"], 4))
+map_name = generate_random_map(4, 0.8)
+
+# print(is_valid(MAPS["4x4"], 4))
+
+
+# Grid World Environment
+class GridWorldEnv(discrete.DiscreteEnv):
+    """
+    Default grid world environment:
+        SFTF
+        FTTF
+        FTFG
+        FFFF
+
+    S: Start cell
+    F: Free cell, reward=-1
+    T: Traffic cell, reward=-10
+    G: Goal, reward=+100
+
+    The episode ends when you reach the Goal.
+    """
+
+    metadata = {'render.modes': ['human', 'ansi']}
+
+    def __init__(self, desc=None, map_name="4x4", stochastic=True):
+        """
+        desc: map description, list[list[char code]]
+        map_name: name of the map
+        stochastic: whether to use stochastic transitions
+        """
+
+        if desc is None and map_name is None:
+            desc = generate_random_map()
+        elif desc is None:
+            desc = MAPS[map_name]
+        self.desc = desc = np.asarray(desc, dtype='c')
+        self.nrow, self.ncol = nrow, ncol = desc.shape
+        self.reward_range = (0, 1)
+
+        # number of actions and states
+        nA = 4
+        nS = nrow * ncol
+
+        # P - transition probability matrix
+        P = {s : {a : [] for a in range(nA)} for s in range(nS)}
+        self.TransitProb = np.zeros((nA, nS + 1, nS + 1))
+        self.TransitReward = np.zeros((nS + 1, nA))
+
+        def to_s(row, col):
+            return row * ncol + col
+
+        def inc(row, col, a):
+            if a == LEFT:
+                col = max(col-1, 0)
+            elif a ==DOWN:
+                row = min(row+1, nrow-1)
+            elif a == RIGHT:
+                col = min(col+1, ncol-1)
+            elif a == UP:
+                row = max(row-1, 0)
+            return (row, col)
+
+        
+GridWorldEnv(map_name)
